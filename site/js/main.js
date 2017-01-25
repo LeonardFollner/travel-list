f708baa2dec1e051165827c8bdbb6f964b4f063d
Leonard Follner <Leonard.Follner@gmail.com>
2017-01-25 23:05:42 +0100

    [FEATURE] added Cookies

--- site/js/main.js
+++ site/js/main.js
@@ -1,11 +1,82 @@
-var renderList;
+var newList;
+var chooseList;
 
 $(document).ready(function () {
   var data = {};
+  var allListData = loadListNames();
+  var currentListId;
+  var currentListData = data;
+
   var checkedItems = 0;
   var totalItemNumber = 0;
 
   var table = $("#pack-list");
+
+  newList = function () {
+    // enable all items
+    $.each(currentListData.lists, function () {
+      if (this.enabled) {
+        $.each(this.items, function () {
+          this.enabled = true;
+        });
+      }
+    });
+
+    currentListId = 0;
+    if (allListData !== undefined) {
+      currentListId = allListData.length;
+    }
+
+    var listName = $("#list-info__name").val();
+    var listLocation = $("#list-info__location").val();
+    var listStart = $("#list-info__start").val();
+    var listEnd = $("#list-info__end").val();
+
+    currentListData.id = currentListId;
+    currentListData.name = listName;
+    currentListData.location = listLocation;
+    currentListData.start = listStart;
+    currentListData.end = listEnd;
+
+    var newListMeta = [];
+    newListMeta.push(listName);
+    newListMeta.push(currentListId);
+    if (allListData === undefined) {
+      allListData = [];
+    }
+    allListData.push(newListMeta);
+    saveListNames();
+
+    saveCurrentList();
+    renderList();
+
+  };
+
+  chooseList = function (elem) {
+    currentListId = $(elem).data("id");
+    var listCookie = loadList(currentListId);
+    // listCookie = [[name, id, location, start, end], [items]]
+
+    currentListData.id = listCookie[0][1];
+    currentListData.name = listCookie[0][0];
+    currentListData.location = listCookie[0][2];
+    currentListData.start = listCookie[0][3];
+    currentListData.end = listCookie[0][4];
+
+    $.each(listCookie[1], function () {
+      // this = [id, checked, amount, notes]
+      var itemId = this[0];
+
+      // enable category
+      findListById(itemId.split("_")[0]).enabled = true;
+
+      findItemById(itemId).enabled = true;
+      findItemById(itemId).checked = this[1];
+      findItemById(itemId).amount = this[2];
+      findItemById(itemId).notes = this[3];
+    });
+    renderList();
+  };
 
   function init() {
     $.ajax({
@@ -14,15 +85,12 @@ $(document).ready(function () {
       async: false,
       success: function (response) {
         data = response;
+        currentListData = response;
       }
     });
-    console.log(data);
-
-    $("#list-info__name").val(data.name);
-    $("#list-info__location").val(data.location);
 
     var options = $("#list-info__lists");
-    $.each(data.lists, function () {
+    $.each(currentListData.lists, function () {
       var listName = this.name;
       var listId = this.id;
       var listEnabled = this.enabled;
@@ -43,12 +111,22 @@ $(document).ready(function () {
       var listID = $(this).parent().parent().data("id");
       findListById(listID).enabled = $(this).prop("checked");
     });
+
+    // load previous lists
+    if (allListData !== undefined) {
+      var listsTable = $("#lists-list");
+      $.each(allListData, function () {
+        // this = [name, id]
+        var listNameRow = $("<tr class='lists-list__list' onclick='chooseList(this);'><td>" + this[0] + "</td></tr>").data("id", this[1]);
+        listsTable.append(listNameRow);
+      });
+    }
   }
 
   init();
 
   // generates the package-list for a given data-set
-  function generateTable(list) {
+  function generateTable() {
     var theader = $("<thead></thead>");
     var categoryHeaderCell = $("<th>Kategorie</th>");
     var itemHeaderCell = $("<th>Mitnehmen</th>");
@@ -65,12 +143,10 @@ $(document).ready(function () {
     table.append(theader);
 
     // appending each category
-    $.each(list.lists, function () {
+    $.each(currentListData.lists, function () {
       if (this.enabled) {
         var listName = this.name;
         var items = this.items;
-
-        totalItemNumber += items.length;
 
         var tbody = $("<tbody></tbody>")
           .attr("id", "pack-list__" + listName)
@@ -78,41 +154,45 @@ $(document).ready(function () {
         table.append(tbody);
 
         $.each(items, function (index) {
-          var row = $("<tr></tr>")
-            .data("id", this.id);
+          if (this.enabled) {
+            totalItemNumber += 1;
 
-          // category header
-          if (index === 0) {
-            var categoryCell = $("<th class='list__name'>" + listName + "</th>")
-              .attr("rowspan", items.length);
-            row.append(categoryCell);
+            var row = $("<tr></tr>")
+              .data("id", this.id);
+
+            // category header
+            if (index === 0) {
+              var categoryCell = $("<th class='list__name'>" + listName + "</th>")
+                .attr("rowspan", items.length);
+              row.append(categoryCell);
+            }
+
+            if (this.checked) {
+              checkedItems += 1;
+            }
+
+            // single item
+            var nameCell = $("<td>" + this.name + "</td>");
+            var amountInputCell = $("<input type='number' class='item__amount' min='1'>").val(this.amount);
+            var amountCell = $("<td></td>");
+            amountCell.append(amountInputCell);
+            var checkedCell = $("<td></td>");
+            var checkedCheckboxCell = $("<input type='checkbox' class='item__checkbox'>")
+              .attr("checked", this.checked);
+            checkedCell.append(checkedCheckboxCell);
+            var notesCell = $("<td></td>");
+            var notesInputCell = $("<input type='text' class='item__notes'>").val(this.notes);
+            notesCell.append(notesInputCell);
+            var deleteCell = $("<td class='item__delete'><i class='fa fa-trash-o' aria-hidden='true'></i></td>");
+
+            // append all cells to the row
+            row.append(nameCell);
+            row.append(amountCell);
+            row.append(checkedCell);
+            row.append(notesCell);
+            row.append(deleteCell);
+            tbody.append(row);
           }
-
-          if (this.checked) {
-            checkedItems += 1;
-          }
-
-          // single item
-          var nameCell = $("<td>" + this.name + "</td>");
-          var amountInputCell = $("<input type='number' class='item__amount' min='1'>").val(this.amount);
-          var amountCell = $("<td></td>");
-          amountCell.append(amountInputCell);
-          var checkedCell = $("<td></td>");
-          var checkedCheckboxCell = $("<input type='checkbox' class='item__checkbox'>")
-            .attr("checked", this.checked);
-          checkedCell.append(checkedCheckboxCell);
-          var notesCell = $("<td></td>");
-          var notesInputCell = $("<input type='text'>").val(this.notes);
-          notesCell.append(notesInputCell);
-          var deleteCell = $("<td class='item__delete'><i class='fa fa-trash-o' aria-hidden='true'></i></td>");
-
-          // append all cells to the row
-          row.append(nameCell);
-          row.append(amountCell);
-          row.append(checkedCell);
-          row.append(notesCell);
-          row.append(deleteCell);
-          tbody.append(row);
         });
       }
     });
@@ -122,11 +202,11 @@ $(document).ready(function () {
     var singleIds = id.split("_");
     var listId = singleIds[0];
     var itemId = singleIds[1];
-    return data.lists[listId].items[itemId];
+    return currentListData.lists[listId].items[itemId];
   }
 
   function findListById(id) {
-    return data.lists[id];
+    return currentListData.lists[id];
   }
 
   function updateProgressBar() {
@@ -140,9 +220,15 @@ $(document).ready(function () {
       .css("background-color", color);
   }
 
-  renderList = function () {
+  function renderList() {
+    console.log(currentListData);
+
     $(".list-info").hide();
-    generateTable(data);
+    $("#lists-list").hide();
+
+    $("#list-name").text(currentListData.name);
+
+    generateTable(currentListData);
     updateProgressBar();
 
     // delete items
@@ -156,7 +242,9 @@ $(document).ready(function () {
       if (findItemById(itemId).checked) {
         checkedItems -= 1;
       }
+
       updateProgressBar();
+      saveCurrentList();
     });
 
     // check or uncheck items
@@ -170,12 +258,77 @@ $(document).ready(function () {
         checkedItems -= 1;
       }
       updateProgressBar();
+      saveCurrentList();
     });
 
     // change item amount
     $(".item__amount").on("change", function () {
       var item = $(this).parent().parent();
       findItemById(item.data("id")).amount = $(this).val();
+      saveCurrentList();
     });
-  };
+
+    // change item notes
+    $(".item__notes").on("change", function () {
+      var item = $(this).parent().parent();
+      findItemById(item.data("id")).notes = $(this).val();
+      saveCurrentList();
+    });
+  }
+
+  function saveCurrentList() {
+    saveToId(currentListId);
+  }
+
+  function saveToId(id) {
+    var cookie = [];
+    var listInfo = [];
+    var listData = [];
+
+    listInfo.push(currentListData.name);
+    listInfo.push(currentListData.id);
+    listInfo.push(currentListData.location);
+    listInfo.push(currentListData.start);
+    listInfo.push(currentListData.end);
+
+    $.each(currentListData.lists, function () {
+      if (this.enabled) {
+        $.each(this.items, function () {
+          if (this.enabled) {
+            var itemCookie = [];
+            itemCookie.push(this.id);
+            itemCookie.push(this.checked);
+            itemCookie.push(this.amount);
+            itemCookie.push(this.notes);
+
+            listData.push(itemCookie);
+          }
+        });
+      }
+    });
+
+    cookie.push(listInfo);
+    cookie.push(listData);
+
+    Cookies.set("list-" + id, cookie);
+  }
+
+  function loadList(id) {
+    return Cookies.getJSON("list-" + id);
+  }
+
+  function saveListNames() {
+    Cookies.set("lists", allListData);
+  }
+
+  function loadListNames() {
+    return Cookies.getJSON("lists");
+  }
 });
+
+function clearAllCookies() {
+  Cookies.remove("lists");
+  for (var i = 0; i < 50; i++) {
+    Cookies.remove("list-" + i);
+  }
+}
